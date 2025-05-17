0adc75772dca8d3ad5da24484243f69e0a3b95d1
rajesh <my_identity@mac.com>
2025-05-17 14:55:53 -0400

    Add dedicated IconFix.css to fix Material Icons rendering

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './components/UI/IconFix.css';
 
 /**
  * Render the application to the DOM.
