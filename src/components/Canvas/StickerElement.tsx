b3ea9d057830f7af0bc2df651c3b524bbbd05fa7
rajesh <my_identity@mac.com>
2025-05-18 01:33:06 -0400

    Fix sticker element rendering to support emoji stickers

--- src/components/Canvas/StickerElement.tsx
+++ src/components/Canvas/StickerElement.tsx
@@ -7,30 +7,42 @@ interface StickerElementProps {
 }
 
 const StickerElement: React.FC<StickerElementProps> = ({ element, isSelected, onClick }) => {
+  console.log('Rendering StickerElement with data:', element);
+  
   return (
     <div
       style={{
         position: 'absolute',
-        left: element.x,
-        top: element.y,
-        width: element.width || 100,
-        height: element.height || 100,
+        left: element.position?.x || element.x || 0,
+        top: element.position?.y || element.y || 0,
+        width: element.size?.width || element.width || 100,
+        height: element.size?.height || element.height || 100,
         border: isSelected ? '2px dashed #1976d2' : 'none',
         pointerEvents: 'all',
         cursor: 'move',
-        zIndex: element.zIndex || 1
+        zIndex: element.zIndex || 1,
+        backgroundColor: element.color || 'transparent',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
       onClick={onClick}
     >
-      <img
-        src={element.src}
-        alt=""
-        style={{
-          width: '100%',
-          height: '100%',
-          objectFit: 'contain'
-        }}
-      />
+      {element.emoji ? (
+        <span style={{ fontSize: '3rem' }}>{element.emoji}</span>
+      ) : element.src ? (
+        <img
+          src={element.src}
+          alt=""
+          style={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'contain'
+          }}
+        />
+      ) : (
+        <div style={{ fontSize: '1rem', color: '#666' }}>Sticker</div>
+      )}
     </div>
   );
 };
